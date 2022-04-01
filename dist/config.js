4fb4475c2c4971c02599bf5162001cd4b46630cc
Ruben Shekspir <shekspir55@gmail.com>
2022-04-01 17:28:37 +0400

    Removed dist and node modules dir

--- dist/config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.secretKey = void 0;
-const secretKey = "HtzWKV6oiuT1OU8dsQCqqcuYvfY0K7";
-exports.secretKey = secretKey;
-//# sourceMappingURL=config.js.map
